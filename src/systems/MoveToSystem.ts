1fd38e46d7d8ed5b43f70b8618ad2ad46589b4b6
Skeller-perso <keller.samuel1@gmail.com>
2025-08-01 17:14:17 +0200

    Normalement c'est bon

--- src/systems/MoveToSystem.ts
+++ src/systems/MoveToSystem.ts
@@ -1,14 +1,14 @@
 import { ECS } from "../ECS";
 import PositionComponent from "../components/PositionComponent";
 import MoveToIntentComponent from "../components/intents/MoveToIntentComponent";
-import MoveIntentComponent from "../components/intents/MoveIntentComponent";
-import { TMap } from "../types/TMap";
-import { directionBetweenPoints } from "../utils/PathUtils";
+import { TileMap } from "../types/TileMap";
 import CanMoveComponent from "../components/CanMoveComponent";
 import PathfindingUtils from "../utils/PathfindingUtils";
 import PathComponent from "../components/PathComponent";
 
-export function MoveToSystem(ecs: ECS, map: TMap) {
+export default function MoveToSystem(ecs: ECS, map: TileMap) {
+    // Instanciation unique de PathfindingUtils pour ce tick
+    const pathfinder = new PathfindingUtils(map);
     const entities = ecs.getEntitiesWith(PositionComponent, MoveToIntentComponent, CanMoveComponent);
 
     for (const e of entities) {
@@ -16,16 +16,19 @@ export function MoveToSystem(ecs: ECS, map: TMap) {
         const moveTo = ecs.getComponent(e, MoveToIntentComponent)!;
         let pathComponent = ecs.getComponent(e, PathComponent);
 
-        const pathLastStep = pathComponent?.path[pathComponent.path.length - 1]
-        // Si pas de path ou cible différente, recalcule le chemin
-        if (!pathComponent ||
+        // Cas où le chemin doit être recalculé :
+        // - Pas de PathComponent
+        // - Chemin vide
+        // - La cible a changé
+        const shouldRecalculatePath =
+            !pathComponent ||
             !pathComponent.path.length ||
-            (
-                moveTo.target.x !== pathLastStep!.x ||
-                moveTo.target.y !== pathLastStep!.y
-            )
-        ) {
-            const pathSteps = new PathfindingUtils(map).getTilesPathFinding(pos, moveTo.target);
+             (
+                moveTo.target.x !== pathComponent.path[pathComponent.path.length - 1].x ||
+                moveTo.target.y !== pathComponent.path[pathComponent.path.length - 1].y
+            );
+        if (shouldRecalculatePath) {
+            const pathSteps = pathfinder.getTilesPathFinding(pos, moveTo.target);
             if (!pathSteps || pathSteps.length === 0) {
                 ecs.removeComponent(e, MoveToIntentComponent);
                 ecs.removeComponent(e, PathComponent);
@@ -34,6 +37,5 @@ export function MoveToSystem(ecs: ECS, map: TMap) {
             ecs.addComponent(e, new PathComponent(pathSteps));
             pathComponent = ecs.getComponent(e, PathComponent);
         }
-
     }
 }
